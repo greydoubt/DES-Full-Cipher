8ffd111d338efa2c2c927ff7a55739a03386d3cb
greydoubt <seanalexander@uchicago.edu>
2020-03-30 02:18:52 +0000

    S-Boxes implemented

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+
+
 print("PART ONE")
 
 # Helpful byte and bit processing
@@ -127,7 +130,10 @@ print('inputbitstrperm: ',inputbitstrperm)
 
 
 print("TEST PART TWO:")
+# add this to final product
 
+
+# NOTE: this says BIT not BYTE
 def Permutation(bitstr, permorderlist):
     permedbitstr = ''
     
@@ -143,8 +149,211 @@ test_inputbitstr = byteseq2binstr(inputbytes)
 print('inputbitstr: ', test_inputbitstr)
 
 # INITIAL PERMUTATION: 
-test_out = Permutation(test_inputbitstr, BookInitPermOrder)
+test_init = Permutation(test_inputbitstr, BookInitPermOrder)
 # INVERT (SHOULD BE THE SAME AS INITIAL BIT STRING)
-test_inv = Permutation(test_out, BookInvInitPermOrder)
-print(test_inputbitstr == test_inv)
+test_inverse = Permutation(test_init, BookInvInitPermOrder)
+print(test_inputbitstr == test_inverse)
 
+print("PART THREE:")
+
+somestring = test_init
+
+def split(somestring):
+	return somestring[:int(len(test_init)/2)],somestring[int(len(test_init)/2):]
+
+LHS, RHS = split(somestring)
+print (LHS, RHS)
+
+
+# EXPANSION FUNCTION:
+
+E_TABLE = [32,1,2,3,4,5,4,5,6,7,8,9,8,9,10,11,12,13,12,13,14,15,16,17,16,17,18,19,20,21,20,21,22,23,24,25,24,25,26,27,28,29,28,29,30,31,32,1]
+
+def Expansion(inputbitstr32, e_table):
+    # the input string is 32 bits long and the output string will be 48 bits long or
+    # to be more exact, it will be as long as the e_table (which is 48 bits for DES)
+ 
+    # create output empty string
+    outputbitstr48 = ''
+
+    for i in e_table:
+      outputbitstr48 += inputbitstr32[i-1]
+
+    # add proper elements from the inputbitstr32 according to the e_table
+    
+    return outputbitstr48
+
+# test expansion function:
+expanded = Expansion(RHS, E_TABLE)
+print("Expanded: " + str(expanded))
+
+
+# XOR FUNCTION:
+def XORbits(bitstr1, bitstr2):
+    # Both bit strings should be the same length
+    # output will be a string with the same length
+    xor_result = '' # this was None originally
+
+    for i in range(len(bitstr1)):
+      if bitstr1[i] == bitstr2[i]:
+        xor_result += '0'
+      else:
+        xor_result += '1'
+    
+    return xor_result
+
+# test XOR function:
+#test_xor_result = XORbits('0001', '0000')
+
+#print(test_xor_result)
+
+
+# intermediate permutation table:
+# this runs AFTER the S-Box 
+MiddlePermOrder = [16,7,20,21,29,12,
+                  28,17,1,15,23,26,5,
+                  18,31,10,2,8,24,14,
+                  32,27,3,9,19,13,30,
+                  6,22,11,4,25]
+#print(MiddlePermOrder)
+
+# as per the notes, this is the same as the function already written, just using a different perm box 
+
+test_middleperm = Permutation(expanded, MiddlePermOrder)
+print(test_middleperm)
+
+# prior to going into sbox, slice it up:
+
+boxstr = expanded
+eightboxes = list(map(''.join, zip(*[iter(boxstr)]*6)))
+print(eightboxes)
+
+
+#blocks = [prebox[:6], prebox[6:12], prebox[12:18], prebox[18:24], prebox[24:30], prebox[30:36], prebox[36:42], prebox[42:]]
+
+SBOX = [
+# Box-1
+[
+[14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7],
+[0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8],
+[4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0],
+[15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13]
+],
+# Box-2
+
+[
+[15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10],
+[3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5],
+[0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15],
+[13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9]
+],
+
+# Box-3
+
+[
+[10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8],
+[13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1],
+[13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7],
+[1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12]
+
+],
+
+# Box-4
+[
+[7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15],
+[13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9],
+[10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4],
+[3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14]
+],
+
+# Box-5
+[
+[2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9],
+[14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6],
+[4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14],
+[11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3]
+],
+# Box-6
+
+[
+[12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11],
+[10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8],
+[9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6],
+[4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13]
+
+],
+# Box-7
+[
+[4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1],
+[13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6],
+[1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2],
+[6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12]
+],
+# Box-8
+
+[
+[13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7],
+[1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2],
+[7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8],
+[2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11]
+]
+
+]
+
+DECtoBIN4 = {0: '0000',
+            1: '0001',
+            2: '0010',
+            3: '0011',
+            4: '0100',
+            5: '0101',
+            6: '0110',
+            7: '0111',
+            8: '1000',
+            9: '1001',
+            10: '1010',
+            11: '1011',
+            12: '1100',
+            13: '1101',
+            14: '1110',
+            15: '1111'}
+
+DECtoBIN2 = {0: '00',
+            1: '01',
+            2: '10',
+            3: '11'}            
+
+
+print(eightboxes[7])
+print(eightboxes[7][0]+eightboxes[7][-1])
+print(eightboxes[7][1:5])
+
+# this will be called in a for loop from 0-7
+def sbox_lookup(input6bitstr, sboxindex):
+    # find the row index (0-3)
+    # find the col index (0-7)
+    
+    rowbin = input6bitstr[0]+input6bitstr[-1] # this is 00 through 11; get first/last chars
+    colbin = input6bitstr[1:5] # get middle 4 chars
+    
+    
+    row = list(DECtoBIN4.keys())[list(DECtoBIN2.values()).index(rowbin)]
+
+    col = list(DECtoBIN4.keys())[list(DECtoBIN4.values()).index(colbin)]
+
+    sbox_value = SBOX[sboxindex][row][col]
+    
+    # Need to convert to 4 bits binary string    
+    return DECtoBIN4.get(sbox_value)
+
+# test case: 011011 for S5 will be [2,14] = 9
+print(SBOX[4][1][13])
+
+# to go from key to value:
+#print(DECtoBIN4.get(1))
+
+#print(eightboxes[7][])
+
+print(sbox_lookup('011011', 4))
+
+#to go from value to key:
+#print(list(DECtoBIN4.keys())[list(DECtoBIN4.values()).index('1100')])
